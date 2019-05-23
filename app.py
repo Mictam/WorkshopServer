5bd2a18a45348e9b0cac7b1cd46979b596b41ee9
mictam <matula.mikolaj@gmail.com>
2019-05-23 15:02:09 +0200

    - Queue added

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import jsonify, request, redirect
+from Queue.queue import __Queue
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,8 +9,17 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route("/add_move",  methods=["POST"])
-def add_move():
-    if not request.json:
-        return "Account data not provided", 400
+@app.route("/add_move", methods=["GET"])
+def add_move(Q = __Queue):
+    if "id" not in request.args:
+        return "Move ID not provided in request", 400
+    id = request.args.get('id')
+    __Queue.add_to_queue(Q, id)
+    print(Q.get_queue(Q))
+    return  "success", 200
+
+
+
+
+
 
