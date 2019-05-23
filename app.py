83cc7d9327c2b3143090bf930d466bc03bd40b93
mictam <matula.mikolaj@gmail.com>
2019-05-23 14:00:27 +0200

    -add_move endpoit

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask import jsonify, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route("/add_move",  methods=["POST"])
+def add_move():
+    if not request.json:
+        return "Account data not provided", 400
+
